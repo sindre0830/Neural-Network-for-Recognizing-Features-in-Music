fc277a33a9b9059065c263f106a4ad38ae1b7b6d
sindre0830 <sindre0830@gmail.com>
2022-02-09 18:37:44 +0100

    [#42] add initial GET method to SongAnalysis class

--- NN/API/main.py
+++ NN/API/main.py
@@ -5,7 +5,14 @@ app = flask.Flask("NN-Internal-API")
 api = flask_restful.Api(app)
 
 class SongAnalysis(flask_restful.Resource):
-    # methods...
-    pass
+    def get(self):
+        youtubeID = flask.request.args.get(key="id")
+
+        if youtubeID == None:
+            return {"Message": "Error occured, requires a YouTube ID."}, 400
+
+        return {"Message": youtubeID}, 200
 
 api.add_resource(SongAnalysis, "/analyze")
+
+app.run()
