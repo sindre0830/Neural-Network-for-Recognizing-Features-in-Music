d572667b45141480d49b2a4e78b821e3bfa3997b
sindre0830 <sindre0830@gmail.com>
2022-02-09 17:51:29 +0100

    [#42] add SongAnalysis class

--- NN/API/main.py
+++ NN/API/main.py
@@ -3,3 +3,9 @@ import flask_restful
 
 app = flask.Flask("NN-Internal-API")
 api = flask_restful.Api(app)
+
+class SongAnalysis(flask_restful.Resource):
+    # methods...
+    pass
+
+api.add_resource(SongAnalysis, "/analyze")
