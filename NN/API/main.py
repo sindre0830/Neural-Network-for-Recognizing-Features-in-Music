090034a73454be1241495b28dacc4b65b06ff07d
sindre0830 <sindre0830@gmail.com>
2022-02-14 17:07:33 +0100

    [#42] Add uptime to diagnosis

--- NN/API/main.py
+++ NN/API/main.py
@@ -1,13 +1,21 @@
 import flask
 import flask_restful
+import time
+import math
+
+start_time = time.time()
 
 app = flask.Flask("NN-Internal-API")
 api = flask_restful.Api(app)
 
 
+def getUptime():
+    return ("%i seconds" % math.floor(time.time() - start_time))
+
+
 class Diagnosis(flask_restful.Resource):
     def get(self):
-        return {}, 200
+        return {"Uptime": getUptime()}, 200
 
 
 api.add_resource(Diagnosis, "/v1/diag")
