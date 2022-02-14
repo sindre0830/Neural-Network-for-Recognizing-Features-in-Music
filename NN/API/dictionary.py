5f9a294817fa40e3eb60e261e98abea5086e7f1d
rickarl <rickarl@stud.ntnu.no>
2022-02-14 11:32:57 +0100

    Fixed end-of-file newlines

--- NN/API/dictionary.py
+++ NN/API/dictionary.py
@@ -9,10 +9,13 @@ DONE = 'DONE'
 SUCCESS = 'SUCCESS'
 FAILED = 'FAILED'
 
+
 # Print operation that allows status message on the same line.
 def printOperation(message):
     print('{:<60s}'.format(message), end="", flush=True)
 
+
 # Print divider in console.
 def printDivider():
     print("\n") 
+    
