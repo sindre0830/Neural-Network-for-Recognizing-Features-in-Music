1027d326cad62c31494164c1af1595ea722db77d
sindre0830 <sindre0830@gmail.com>
2022-02-14 17:16:19 +0100

    [#42] Convert to modern Flask formatting

--- NN/API/main.py
+++ NN/API/main.py
@@ -1,23 +1,16 @@
 import flask
-import flask_restful
 import time
 import math
 
 start_time = time.time()
 
 app = flask.Flask("NN-Internal-API")
-api = flask_restful.Api(app)
 
 
 def getUptime():
     return ("%i seconds" % math.floor(time.time() - start_time))
 
 
-class Diagnosis(flask_restful.Resource):
-    def get(self):
-        return {"Uptime": getUptime()}, 200
-
-
-api.add_resource(Diagnosis, "/v1/diag")
-
-app.run()
+@app.route("/v1/diag")
+def diagnosis():
+    return {"Uptime": getUptime()}
