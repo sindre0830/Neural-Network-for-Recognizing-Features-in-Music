e6086887802f773c49991869ae9ca95bc1d7ea22
Maren <marengrindal@gmail.com>
2022-02-16 17:48:53 +0100

    [#35] Add structure for storing song data

--- API/Results/structure.go
+++ API/Results/structure.go
@@ -1,7 +1,17 @@
 package results
 
+// Song stores information about a song
+type Song struct {
+	title       string
+	beats       []float64
+	parts       []map[string]interface{}
+	arrangement []map[string]interface{}
+	startTime   float64
+}
+
 // Result stores the result of the song analysis
 type Result struct {
+	title string
 	beats []float64
 	parts []struct {
 		length int
