c2f9170a3932cdcfcd33c0b70af9ff92df811477
Maren <marengrindal@gmail.com>
2022-02-16 18:39:12 +0100

    Add global database object

--- API/Database/db.go
+++ API/Database/db.go
@@ -10,25 +10,28 @@ import (
 	"google.golang.org/api/option"
 )
 
+// Global object for db connection
+var Firestore Database
+
 // Database structure contains db variables
 type Database struct {
-	ctx    context.Context
-	client *firestore.Client
+	Ctx    context.Context
+	Client *firestore.Client
 }
 
 // Setup sets up the database.
 func (db *Database) Setup() error {
 	// initialization
-	db.ctx = context.Background()
+	db.Ctx = context.Background()
 
 	// connect to Firebase with the service account key
-	opt := option.WithCredentialsFile("./serviceAccountKey.json")
+	opt := option.WithCredentialsFile("./test-aa354-firebase-adminsdk-iktfk-7cd7559143.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		return err
 	}
 
-	db.client, err = app.Firestore(db.ctx)
+	db.Client, err = app.Firestore(db.Ctx)
 	if err != nil {
 		return err
 	}
@@ -39,7 +42,7 @@ func (db *Database) Setup() error {
 // Get a document from a collection.
 func (db *Database) Get(collection string, id string) (map[string]interface{}, error) {
 	// find the document with the specific ID
-	dsnap, err := db.client.Collection(collection).Doc(id).Get(db.ctx)
+	dsnap, err := db.Client.Collection(collection).Doc(id).Get(db.Ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -52,7 +55,7 @@ func (db *Database) Get(collection string, id string) (map[string]interface{}, e
 func (db *Database) GetAll(collection string) ([]map[string]interface{}, error) {
 	var data []map[string]interface{}
 	// iterate through collection
-	iter := db.client.Collection(collection).Documents(db.ctx)
+	iter := db.Client.Collection(collection).Documents(db.Ctx)
 	for {
 		el, err := iter.Next()
 		if err == iterator.Done {
@@ -64,17 +67,18 @@ func (db *Database) GetAll(collection string) ([]map[string]interface{}, error) 
 		// add current document to the data slice
 		data = append(data, el.Data())
 	}
+
 	return data, nil
 }
 
 // Add a document to a collection.
 func (db *Database) Add(collection string, id string, data interface{}) error {
-	_, err := db.client.Collection(collection).Doc(id).Set(db.ctx, data)
+	_, err := db.Client.Collection(collection).Doc(id).Set(db.Ctx, data)
 	return err
 }
 
 // Delete a document from a collection.
 func (db *Database) Delete(collection string, id string) error {
-	_, err := db.client.Collection(collection).Doc(id).Delete(db.ctx)
+	_, err := db.Client.Collection(collection).Doc(id).Delete(db.Ctx)
 	return err
 }
