6d49f884f1815161061b0eb35a8c3cc8b287565c
Maren <marengrindal@gmail.com>
2022-02-21 18:36:34 +0100

    [#37] Add decoding of body to structure

--- API/SongAnalysis/songAnalysis.go
+++ API/SongAnalysis/songAnalysis.go
@@ -1,6 +1,8 @@
 package songanalysis
 
 import (
+	"encoding/json"
+	debug "main/Debug"
 	"net/http"
 	"strings"
 )
@@ -11,11 +13,41 @@ func post(w http.ResponseWriter, r *http.Request) {
 	path := strings.Split(r.URL.Path, "/")
 	if len(path) != 2 {
 		var errorMsg debug.Debug
-		debug.Update(
+		errorMsg.Update(
 			http.StatusBadRequest,
 			"post() -> Validating URL",
 			"url validation: incorrect format",
 			"URL format not valid",
 		)
+		errorMsg.Print()
+		return
+	}
+
+	// decode body to song structure
+	var song Song
+	err := json.NewDecoder(r.Body).Decode(&song)
+	if err != nil {
+		var errorMsg debug.Debug
+		errorMsg.Update(
+			http.StatusBadRequest,
+			"post() -> Decoding body",
+			err.Error(),
+			"JSON format not valid",
+		)
+		errorMsg.Print()
+		return
+	}
+
+	// check if a link is sent
+	if len(song.Link) <= 0 {
+		var errorMsg debug.Debug
+		errorMsg.Update(
+			http.StatusBadRequest,
+			"post() -> Parsing body",
+			"json: not a valid json format",
+			"JSON format not valid",
+		)
+		errorMsg.Print()
+		return
 	}
 }
