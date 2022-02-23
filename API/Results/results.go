01e7651cc7991ec69daa720229d160596119a6e4
Maren <marengrindal@gmail.com>
2022-02-23 07:51:57 +0100

    [#36] Add URL validation

--- API/Results/results.go
+++ API/Results/results.go
@@ -5,13 +5,15 @@ import (
 	database "main/Database"
 	debug "main/Debug"
 	"net/http"
+	"strings"
 )
 
-// getResults gets all song results from the database
-func getResults(w http.ResponseWriter) {
+// get gets all song results from the database.
+func get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 
 	data := make([]map[string]interface{}, 0)
+	// get all documents from the database
 	data, err := database.Firestore.GetAll("songs")
 	if err != nil {
 		var errorMsg debug.Debug
@@ -27,4 +29,35 @@ func getResults(w http.ResponseWriter) {
 
 	// encode data
 	json.NewEncoder(w).Encode(data)
+}
+
+// update updates a song in the database.
+func update(w http.ResponseWriter, r *http.Request) {
+	// validate URL
+	path := strings.Split(r.URL.Path, "/")
+	if len(path) != 3 {
+		var errorMsg debug.Debug
+		errorMsg.Update(
+			http.StatusBadRequest,
+			"post() -> Validating URL",
+			"url validation: incorrect format",
+			"URL format not valid",
+		)
+		errorMsg.Print()
+		return
+	}
+
+	// make sure an ID is provided by the user
+	id := path[2]
+	if len(id) < 1 {
+		var errorMsg debug.Debug
+		errorMsg.Update(
+			http.StatusBadRequest,
+			"post() -> Validating URL",
+			"url validation: incorrect format",
+			"URL format not valid",
+		)
+		errorMsg.Print()
+		return
+	}
 }
