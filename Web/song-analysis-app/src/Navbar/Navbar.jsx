ac65e13ccf704c4b9d7fac1dca77d808ef89b037
Maren <marengrindal@gmail.com>
2022-02-25 11:49:09 +0100

    [#61] Modify menu so it can be toggled on and off

--- Web/song-analysis-app/src/Navbar/Navbar.jsx
+++ Web/song-analysis-app/src/Navbar/Navbar.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
+const Menu = () => (
+    <>
+        <ul>
+            <li><a>Add song</a> </li>
+            <li><a>Results</a></li>
+            <li><a>Status</a> </li>
+        </ul>
+    </>
+)
+
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
         <div>
             <div>
                 <h1>Neural Network for Recognizing Features in Music</h1>
             </div>
             <div>
-                <ul>
-                    <li><a>Add song</a> </li>
-                    <li><a>Results</a></li> 
-                    <li><a>Status</a> </li>
-                </ul>
+                {toggleMenu
+                    ? <button onClick={() => setToggleMenu(false)}>Close</button>
+                    : <button onClick={() => setToggleMenu(true)}>Open</button>}
+                {toggleMenu && (
+                    <div>
+                        <Menu />
+                    </div>
+                )}
             </div>
         </div>
     )
