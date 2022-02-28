9348617925bf310e624bebecbc6d04b7bd070bd2
Maren <marengrindal@gmail.com>
2022-02-28 18:43:58 +0100

    [#62] Add post request of YouTube link

--- Web/song-analysis-app/src/AddSong/AddSong.jsx
+++ Web/song-analysis-app/src/AddSong/AddSong.jsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './addSong.css';
 
 /**
  * Input page.
  */
 const AddSong = () => {
+    const [link, setLink] = useState("");
+    const [message, setMessage] = useState("");
+
+    let submitLink = async (e) => {
+        e.preventDefault();
+        try {
+            const options = {
+                method: 'POST',
+                body: JSON.stringify(link)
+            }
+
+            const res = await fetch('/post', options);
+            if (res.status === 200) {
+                setLink("");
+                setMessage("Link successfully parsed, result will be uploaded to the result page.");
+            } else {
+                setLink("");
+                setMessage("Error when parsing link");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div className='add-song'>
             {/* this is were error/status messages will show up */}
             <div className='add-song__message'>
-                <p>Hei</p>
+                {message
+                    ? <p>{message}</p>
+                    : null
+                }
             </div>
             <div className='add-song__input'>
-                <form>
-                    <input type='text' name='link' placeholder='Input YouTube link...'/>
-                    <button>Submit</button>
+                <form onSubmit={submitLink}>
+                    <input type='text' name='link' placeholder='Input YouTube link...' onChange={(e) => setLink(e.target.value)}/>
+                    <button type='submit'>Submit</button>
                 </form>
             </div>            
         </div>
