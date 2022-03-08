a8b911c30760ecc99331afcc9315dfbd04ad88b1
Maren <marengrindal@gmail.com>
2022-03-08 14:37:57 +0100

    [#69] Add design for small screens

--- Web/song-analysis-app/src/Song/Song.jsx
+++ Web/song-analysis-app/src/Song/Song.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 import './song.css';
 
 /**
- *  Result from one song.
+ *  Displaying of one song.
  */
 const Song = (props) => {
+    const [toggleSong, setToggle] = useState(false);
     return (
         <div className='song'>
-            <h1>{props.value.name}</h1>
+            <div className='song__title'>
+                <hr className={props.value.approved ? 'song__title-approved' : 'song__title-pending'} />
+                <h1>{props.value.name}</h1>
+
+                {/* show approve button if the song is pending */}
+                {!props.value.approved &&
+                    <button>Approve</button>
+                }
+
+                {/* checks if the arrow is going to point up or down*/}
+                {toggleSong
+                    ? <FiChevronUp size={28} onClick={() => setToggle(false)} style={{cursor: 'pointer'}}/>
+                    : <FiChevronDown size={28} onClick={() => setToggle(true)} style={{ cursor: 'pointer' }}/>
+                }
+            </div>
             <hr />
+            <div className='song__result'>
+                {/* displays result */}
+                {toggleSong &&
+                    <div>
+                        <pre>{JSON.stringify(props.value, null, 2)}</pre>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
