32dcecc4c3428caf673b2dccc7b5ddf36227392a
Maren <marengrindal@gmail.com>
2022-03-24 20:51:42 +0100

    Add endpoint paths to dictionary

---
+++ API/Dictionary/dictionary.go
@@ -0,0 +1,8 @@
+package dictionary
+
+var VERSION = "v1"
+
+// path to all endpoints
+var ANALYSIS_PATH = "/" + VERSION + "/analysis"
+var RESULTS_PATH = "/" + VERSION + "/results"
+var DIAG_PATH = "/" + VERSION + "/diag"
