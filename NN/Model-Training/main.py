0a1d77083182c1a47b59f6f68fcf0531b81809db
sindre0830 <sindre0830@gmail.com>
2022-03-30 14:44:23 +0200

    [#4] Add memory growth to GPUs

--- NN/Model-Training/main.py
+++ NN/Model-Training/main.py
@@ -1,42 +1,23 @@
-# remove info and warnings outputted by tensorflow
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-# import local modules
-import model
 # import foreign modules
-import librosa
-import librosa.display
-import matplotlib.pyplot as plt
-import soundfile as sf
-import shutil
-import numpy as np
-import json
+import os
+import tensorflow as tf
 
-chords = ['', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'Cm', 'C#m',
-          'Dm', 'D#m', 'Em', 'Fm', 'F#m', 'Gm', 'G#m', 'Am', 'A#m', 'Bm']
+# suppress info and warnings outputted by tensorflow
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# enable memory growth for gpu devices
+# source: https://stackoverflow.com/a/55541385/8849692
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+if gpu_devices:
+    for device in gpu_devices:
+        tf.config.experimental.set_memory_growth(device, True)
+
 
 # Main program.
 def main():
-    (data, labels) = np.load("Data/dataset.npy", allow_pickle=True)
-
-    print(len(chords))
-
-    print(labels[0])
-    
-    for i in range(0, len(labels)):
-        labels[i] = chords.index(labels[i])
-    
-    print(labels[0])
-
-    print(labels)
-
-    xTrain, xTest, yTrain, yTest = model.prepareData(data, labels, 0.8)
-    print(xTrain.shape)
-    print(xTest.shape)
-    print(yTrain.shape)
-    print(yTest.shape)
+    pass
 
 
-# branch if program is run through 'python main.py'
+# branch if program is run through 'python main.py' and run main program
 if __name__ == "__main__":
     main()
