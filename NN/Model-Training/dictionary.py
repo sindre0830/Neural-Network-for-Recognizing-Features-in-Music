9eee0323bbcb4e14eace1ba09c821442a80691c2
sindre0830 <sindre0830@gmail.com>
2022-03-30 14:45:51 +0200

    [#4] Add dictionary.py

---
+++ NN/Model-Training/dictionary.py
@@ -0,0 +1,31 @@
+# dataset names
+LABEL_NAMES = ['', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'Cm', 'C#m', 'Dm', 'D#m', 'Em', 'Fm', 'F#m', 'Gm', 'G#m', 'Am', 'A#m', 'Bm']
+# status messages
+DONE = 'DONE'
+SUCCESS = 'SUCCESS'
+FAILED = 'FAILED'
+# set parameters
+SHAPE = (12, 50, 1)
+DATASET_AMOUNT = len(LABEL_NAMES) + 1
+LEARNING_RATE = 0.001
+TRAIN_SIZE = 0.80
+BATCH_SIZE = 512
+EPOCHS = 16
+# paths
+DATASET_PATH = "Data/dataset.npy"
+MODEL_PATH = "Data/model"
+
+
+# Print operation that allows status message on the same line.
+def printOperation(message):
+    print('{:<60s}'.format(message), end="", flush=True)
+
+
+# Print divider in console.
+def printDivider():
+    print("\n")
+
+
+# Print message in console.
+def printMessage(message: str):
+    print(message)
