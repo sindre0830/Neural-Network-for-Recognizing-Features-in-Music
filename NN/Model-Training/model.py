6cbd62321941fc6847ca78fce0c7304aec44736c
sindre0830 <sindre0830@gmail.com>
2022-04-23 08:50:45 +0200

    [#4] Add RandomSearch()

--- NN/Model-Training/model.py
+++ NN/Model-Training/model.py
@@ -10,6 +10,149 @@ import sklearn.metrics
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import itertools
+import random
+import gc
+import datetime
+import os
+
+
+# Perform random search to find the best model.
+def randomSearch(xTrain, xTest, xVal, yTrain, yTest, yVal):
+    layers = [0, 1, 2]
+    filters = [16, 32, 64, 128]
+    units = [32, 64, 128]
+    results = []
+    i = 0
+    N = 20
+    # generate each possible combination, randomize, and choose 20 combinations to test
+    combinations = list(itertools.product(filters, layers, filters, layers, filters, layers, filters, layers, units, layers, units, layers, units))
+    random.shuffle(combinations)
+    combinations = combinations[:N]
+    # iterate through each combination and train each model while saving the results
+    for (initial_filter, conv_layer_1, conv_filter_1, conv_layer_2, conv_filter_2, conv_layer_3, conv_filter_3, 
+         dense_layer_1, dense_units_1, dense_layer_2, dense_units_2, dense_layer_3, dense_units_3) in combinations:
+        i += 1
+        dict.printOperation("Starting work on model " + str(i) + "...")
+        # create and train model based on parameters
+        val_accuracy, val_loss = generateRandomSearchModel(
+            initial_filters=initial_filter,
+            conv_layer_1=conv_layer_1,
+            conv_filters_1=conv_filter_1,
+            conv_layer_2=conv_layer_2,
+            conv_filters_2=conv_filter_2,
+            conv_layer_3=conv_layer_3,
+            conv_filters_3=conv_filter_3,
+            dense_layer_1=dense_layer_1,
+            dense_units_1=dense_units_1,
+            dense_layer_2=dense_layer_2,
+            dense_units_2=dense_units_2,
+            dense_layer_3=dense_layer_3,
+            dense_units_3=dense_units_3,
+            xTrain=xTrain,
+            xTest=xTest, 
+            xVal=xVal,
+            yTrain=yTrain,
+            yTest=yTest,
+            yVal=yVal
+        )
+        # create object and store result and parameters
+        result = {}
+        result["val_accuracy"] = val_accuracy
+        result["val_loss"] = val_loss
+        result["initial_filter"] = initial_filter
+        result["conv_layer_1"] = conv_layer_1
+        result["conv_filter_1"] = conv_filter_1
+        result["conv_layer_2"] = conv_layer_2
+        result["conv_filter_2"] = conv_filter_2
+        result["conv_layer_3"] = conv_layer_3
+        result["conv_filter_3"] = conv_filter_3
+        result["dense_layer_1"] = dense_layer_1
+        result["dense_units_1"] = dense_units_1
+        result["dense_layer_2"] = dense_layer_2
+        result["dense_units_2"] = dense_units_2
+        result["dense_layer_3"] = dense_layer_3
+        result["dense_units_3"] = dense_units_3
+        results.append(result)
+        dict.printMessage(dict.DONE)
+    # sort results by accuracy and convert to string
+    results = sorted(results, key=lambda x: x["val_accuracy"], reverse=True)
+    strResults = ""
+    for val in results:
+        strResults += "val_accuracy: {} \t| val_loss: {} \t| ".format(val["val_accuracy"], val["val_loss"])
+        strResults += "initial_filter: {} \t| conv_layer_1: {} \t| ".format(val["initial_filter"], val["conv_layer_1"])
+        strResults += "conv_filter_1: {} \t| conv_layer_2: {} \t| ".format(val["conv_filter_1"], val["conv_layer_2"])
+        strResults += "conv_filter_2: {} \t| conv_layer_3: {} \t| ".format(val["conv_filter_2"], val["conv_layer_3"])
+        strResults += "conv_filter_3: {} \t| dense_layer_1: {} \t| ".format(val["conv_filter_3"], val["dense_layer_1"])
+        strResults += "dense_units_1: {} \t| dense_layer_2: {} \t| ".format(val["dense_units_1"], val["dense_layer_2"])
+        strResults += "dense_units_2: {} \t| dense_layer_3: {} \t| ".format(val["dense_units_2"], val["dense_layer_3"])
+        strResults += "dense_units_3: {}\n".format(val["dense_units_3"])
+    # Create a folder if it doesn't exist and save the results to a txt file
+    if not os.path.exists("Data/RandomSearch/"):
+        os.makedirs("Data/RandomSearch/")
+    with open("Data/RandomSearch/" + datetime.datetime.now().strftime("%H:%M:%S") + ".txt", "w+") as outfile:
+            outfile.write(strResults)
+    dict.printDivider()
+
+
+# Generate convolutional neural network model and return results. Used for random search.
+def generateRandomSearchModel(
+        initial_filters: int,
+        conv_layer_1: int,
+        conv_filters_1: int,
+        conv_layer_2: int,
+        conv_filters_2: int,
+        conv_layer_3: int,
+        conv_filters_3: int,
+        dense_layer_1: int,
+        dense_units_1: int,
+        dense_layer_2: int,
+        dense_units_2: int, 
+        dense_layer_3: int,
+        dense_units_3: int,
+        xTrain, xTest, xVal, yTrain, yTest, yVal
+    ):
+    model = keras.models.Sequential()
+    # initial layer and conv layers
+    model.add(keras.layers.convolutional.Conv2D(filters=initial_filters, kernel_size=3, input_shape=dict.SHAPE, activation='relu'))
+    for _ in range(conv_layer_1):
+        model.add(keras.layers.convolutional.Conv2D(filters=conv_filters_1, kernel_size=1, activation='relu'))
+    for _ in range(conv_layer_2):
+        model.add(keras.layers.convolutional.Conv2D(filters=conv_filters_2, kernel_size=1, activation='relu'))
+    for _ in range(conv_layer_3):
+        model.add(keras.layers.convolutional.Conv2D(filters=conv_filters_3, kernel_size=1, activation='relu'))
+    # flatten layer and dense layers
+    model.add(keras.layers.core.Flatten())
+    for _ in range(dense_layer_1):
+        model.add(keras.layers.core.Dense(units=dense_units_1, activation='relu'))
+    for _ in range(dense_layer_2):
+        model.add(keras.layers.core.Dense(units=dense_units_2, activation='relu'))
+    for _ in range(dense_layer_3):
+        model.add(keras.layers.core.Dense(units=dense_units_3, activation='relu'))
+    # last layer
+    model.add(keras.layers.core.Dense(units=dict.DATASET_AMOUNT, activation='sigmoid'))
+    model.compile(
+        optimizer=keras.optimizer_v2.adam.Adam(learning_rate=dict.LEARNING_RATE),
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    # fit model by parameters
+    callback = keras.callbacks.EarlyStopping(min_delta=0.01, patience=3)
+    model.fit(
+        xTrain,
+        yTrain,
+        validation_data=(xTest, yTest),
+        batch_size=dict.BATCH_SIZE,
+        epochs=dict.EPOCHS,
+        verbose=0,
+        callbacks=[callback]
+    )
+    # evaluate model and output results
+    results = model.evaluate(xVal, yVal, verbose=0)
+    # delete model and run garbage collection
+    del model
+    gc.collect()
+    return results[0], results[1]
 
 
 # Generate convolutional neural network model.
@@ -39,8 +182,8 @@ def generateModel():
 
 # Function to run model.fit.
 def trainModel(model: keras.models.Sequential, xTrain, xTest, xVal, yTrain, yTest, yVal, verbose_flag):
-    callback = keras.callbacks.EarlyStopping(min_delta=0.01, patience=3)
     # fit model by parameters
+    callback = keras.callbacks.EarlyStopping(min_delta=0.01, patience=3)
     results = model.fit(
         xTrain,
         yTrain,
