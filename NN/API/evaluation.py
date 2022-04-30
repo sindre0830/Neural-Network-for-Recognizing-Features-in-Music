cb4bdf15afa2aae7202cfc2ea01fc70e235cd658
rickarl <rickarl@stud.ntnu.no>
2022-04-30 22:04:08 +0200

    [#82] Improve evaluation.py structure

--- NN/API/evaluation.py
+++ NN/API/evaluation.py
@@ -57,11 +57,14 @@ class Evaluators:
             os.makedirs(dict.RESULTS_SONG_PATH)
 
         # Check if we have existing data for comparison and data does not need to be reprocessed
-        updateJson(dict.ALGORITHM_JSON_PATH, dict.RESULTS_SONG_PATH)
-        algSize = os.path.getsize(dict.ALGORITHM_JSON_PATH)
-        if algSize != 0:  # Add after debugging: or algSize != dataSize
-            with open(dict.ALGORITHM_JSON_PATH, 'r') as f:
-                results = json.loads(f.read())
+        if os.path.exists(dict.ALGORITHM_JSON_PATH):
+            algSize = os.path.getsize(dict.ALGORITHM_JSON_PATH) 
+            if algSize != 0:  # Add after debugging: or algSize != dataSize
+                updateJson(dict.ALGORITHM_JSON_PATH, dict.RESULTS_SONG_PATH)
+                with open(dict.ALGORITHM_JSON_PATH, 'r') as f:
+                    results = json.loads(f.read())
+        else:
+            results ={}
 
         # # Go through dataset
         for id in dataset:
@@ -69,17 +72,16 @@ class Evaluators:
             song = self.Evaluator(id)
             if id in results.keys():
                 print("Using saved result")
-                song.chords = compareChords(dataset[id]["beats"], dataset[id]["chords"], dataset[id]["beats"], results[id]["chords"])
+                song.chords = self.compareChords(dataset[id]["beats"], dataset[id]["chords"], dataset[id]["beats"], results[id]["chords"])
                 song.beats = results[id]["beats"]
             # Get the data we need if not existing
             else:
                 print("Generating from scratch...")
-                song.chords = processChords(self.processed_beats, self.song_buffer, dataset, self.beat_chord_aggregate)
-                meanbeat, song.beats = processBeats(dataset[id]["beats"], id)
-# Above is ok now!
-            createResults(self.detailed_results, id, algorithm = song.chords)
+                song.chords = self.processChords(id, song, dataset)
+                meanbeat, song.beats = self.processBeats(dataset[id]["beats"], id)
+            self.createResults(self.detailed_results, id, algorithm = song.chords)
             print("The result manual is: " + str(song.chords * 100) + chr(37) + " accuracy")
-        output(self.chord_data, self.beat_data, self.detailed_results)
+        
         # Update processed data with trimmed beats
         if(len(self.processed_beats)) > len(dataset):
             json_object = json.dumps(self.processed_beats, indent=3)
@@ -96,7 +98,7 @@ class Evaluators:
             
             # TEST THIS ONE
     # Processes a song and stores the results
-    def processChords(self, song:Evaluator, dataset):
+    def processChords(self, id: str, song:Evaluator, dataset):
         try:
             preprocessing.downloadAudio(id)
         except dict.YoutubeError:
@@ -105,15 +107,15 @@ class Evaluators:
         song.beats = np.array(dataset[id]["beats"])
         song.beats = song.beats[song.beats <= librosa.get_duration(filename=dict.getNativeAudioPath(id))]   # trim timestamps
         chordRecognizer.run(beats=song.beats, verbose=True)
-        createJson(self.beat_chord_aggregate, id, chordRecognizer.chords.tolist(), newBeats.tolist(), "chords", "beats")
-        result = compareChords(song.beats, dataset[id]["chords"], song.beats, chordRecognizer.chords)
+        self.createJson(self.beat_chord_aggregate, id, chordRecognizer.chords.tolist(), song.beats.tolist(), "chords", "beats")
+        result = self.compareChords(song.beats, dataset[id]["chords"], song.beats, chordRecognizer.chords)
         # Save
-        createJson(self.processed_beats, id, dataset[id]["chords"], song.beats.tolist(), "chords", "beats")    # Update processed dataclear
+        self.createJson(self.processed_beats, id, dataset[id]["chords"], song.beats.tolist(), "chords", "beats")    # Update processed dataclear
         json_object = json.dumps(self.processed_beats[id], indent=3)
         with open(dict.TRIMMED_SONGS_PATH + id + '.json', "w+") as outfile:
             outfile.write(json_object)
 
-        createJson(self.song_buffer, id, chordRecognizer.chords.tolist(), song.beats.tolist(), "chords", "beats")
+        self.createJson(self.song_buffer, id, chordRecognizer.chords.tolist(), song.beats.tolist(), "chords", "beats")
         json_object = json.dumps(self.song_buffer[id], indent=3)
         with open(dict.RESULTS_SONG_PATH + id + '.json', "w+") as outfile:
             outfile.write(json_object)
@@ -122,98 +124,127 @@ class Evaluators:
         os.remove(dict.getModifiedAudioPath(id))
         return result
 
-# Handles batch process comparison of database
-def batchHandler(force:bool = False, plot:bool = False):
-    # Make sure we have the dataset parsed
-    if not os.path.exists(dict.PROCESSED_JSON_PATH) or os.path.getsize(dict.PROCESSED_JSON_PATH) < 100:
-        preprocessing.parseJson(dict.JSON_PATH)
-    with open(dict.PROCESSED_JSON_PATH, 'r') as f:
-        dataset = json.loads(f.read())
+    # Updates a dictionary with new key+values
+    def createJson(self, dict, id: str, first, second, fName:str, sName:str):
+        s = {}
+        s[fName] = first
+        s[sName] = second
+        dict[id] = s
 
-    # prep for results
-    if not os.path.exists(dict.RESULTS_PATH):
-        os.makedirs(dict.RESULTS_PATH)
-    if not os.path.exists(dict.RESULTS_SONG_PATH):
-        os.makedirs(dict.RESULTS_SONG_PATH)
 
-    # Check if we have existing data for comparison and data does not need to be reprocessed
-    updateJson(dict.ALGORITHM_JSON_PATH, dict.RESULTS_SONG_PATH)
-    algSize = os.path.getsize(dict.ALGORITHM_JSON_PATH)
-    if algSize != 0:  # Add after debugging: or algSize != dataSize
-        with open(dict.ALGORITHM_JSON_PATH, 'r') as f:
-            results = json.loads(f.read())
-    else:
-        results = {}
+    # Stores accuracy of Chord Algorithm and Neural Network for song
+    def createResults(self, dict, id: str, algorithm: float = None, neural: float = None):
+        s = {}
+        s["algo-accuracy"] = algorithm
+        s["neural-accuracy"] = neural
+        dict[id] = s
 
-    # store our results
-    chord_data = {}
-    beat_data = {}
-    song_buffer = {}
-    beat_chord_aggregate = {}
-    detailed_results = {}
-    # store updated songs
-    processed_beats = {}
-    # # Go through dataset
-    for id in dataset:
-        print(id)
-        if id in results.keys():
-            print("Using saved result")
-            chordresult = compareChords(dataset[id]["beats"], dataset[id]["chords"], dataset[id]["beats"], results[id]["chords"])
-            beatresult = processBeats(dataset[id]["beats"], id)
-        # Get the data we need if not existing
+
+    # Compares two chord arrays based on matching indexes with their timestamp arrays
+    def compareChords(self, gt_timestamp, gt_chord, alg_timestamp, alg_chord):
+        results = 0
+        for idx, timestamp in enumerate(gt_timestamp):
+            near = find_nearest(alg_timestamp, timestamp)
+            algoChord = alg_chord[near]
+            # print(timestamp)              # Debug
+            # print(algoChord + " " + gt_chord[idx])
+            if algoChord == gt_chord[idx]:
+                results += 1
+        # Return number of correct guesses divided by total guesses - can improve
+        temp = results / len(gt_chord)
+        return temp
+
+
+    # records the batch processing results into CSV
+    def output(self, chorddata = None, beatdata = None, detailed = None):
+        if chorddata:
+            df = pd.DataFrame(list(chorddata.items()), columns = ['id', 'result'])   # get it in dataframe form
+            aggregate = pd.cut(df['result'], bins = pd.interval_range(start=0, end=100, periods=10)).value_counts()
+            with open(dict.CHORDRESULTS_CSV_PATH, "w") as f:
+                aggregate.to_csv(f)
+        if detailed:
+            json_object = json.dumps(detailed, indent=3)
+            with open(dict.DETAILED_RESULTS_PATH, "w+") as outfile:
+                outfile.write(json_object)
+        if beatdata:
+            df = pd.DataFrame(list(beatdata.items()), columns = ['id', 'result'])
+            #print(df)
+            aggregate = pd.cut(df['result'].str['accuracy'], bins = pd.interval_range(start=0, end=100, periods=10)).value_counts()
+            with open(dict.BEATRESULTS_CSV_PATH, "w") as f:
+                aggregate.to_csv(f)
+
+
+
+    def processBeats(self, dataset, id):
+        beatRecognizer = beat_algorithm.BeatRecognizer(id)
+        beatRecognizer.run()
+        return self.evaluateBeats(dataset, beatRecognizer.beats)
+
+
+    def evaluateBeats(self, dataset:float, algorithm:float, verbose = False):
+        beatAccuracy = []
+        for previous, item, nxt in previous_and_next(dataset):
+            nearIdx = find_nearest(algorithm, item)
+            distance = item - algorithm[nearIdx]
+            if nxt == None:
+                nxt = previous
+            if previous == None:
+                previous = nxt
+            if distance >= 0:
+                control = abs(item - previous)
+            else:
+                control = abs(item - nxt)
+            # We halve control - at normal, it finds a value matching the control point
+            # to be 0% accurate, even though it's 100% accurate to the control.
+            beatAccuracy.append(100-abs((distance / (control/2))*100))
+            if algorithm[nearIdx] == algorithm[-1]:  # Indicates dataset is longer
+                break
+        meanAccuracy = mean(beatAccuracy)
+        if verbose:
+            print("The average accuracy is: ", meanAccuracy)
+        return meanAccuracy, beatAccuracy
+
+
+    def beatProcess(self, force:bool = False, verbose:bool = False):
+        with open(dict.PROCESSED_JSON_PATH, 'r') as f:
+            dataset = json.loads(f.read())
+        if not os.path.exists(dict.RESULTS_BEATS_PATH):
+            os.makedirs(dict.RESULTS_BEATS_PATH)
+        beat_data = {}
+        updateJson(dict.BEAT_RESULTS_PATH, dict.RESULTS_BEATS_PATH) # avoid error on nonexistent file here
+        beatSize = os.path.getsize(dict.BEAT_RESULTS_PATH)
+        if beatSize != 0:
+            with open(dict.BEAT_RESULTS_PATH, 'r') as f:
+                results = json.loads(f.read())
         else:
-            print("Generating from scratch...")
-            chordresult = processChords(processed_beats, song_buffer, dataset, beat_chord_aggregate)
-            beatresult = processBeats(dataset[id]["beats"], id)
-        chord_data[id] = chordresult*100
-        beat_data[id] = beatresult
-        createResults(detailed_results, id, algorithm = chordresult)
-        print("The result manual is: " + str(chordresult * 100) + chr(37) + " accuracy")
-    output(chord_data, beat_data, detailed_results)
-    # Update processed data with trimmed beats
-    if(len(processed_beats)) > len(dataset):
-        json_object = json.dumps(processed_beats, indent=3)
-        with open(dict.PROCESSED_JSON_PATH, "w+") as outfile:
-            outfile.write(json_object)
-    # Write our new algorithm data to file
-    if(len(song_buffer)) > len(results):
-        print("Writing algorithm results...")
-        json_object = json.dumps(beat_chord_aggregate, indent=3)
-        with open(dict.ALGORITHM_JSON_PATH, "w+") as outfile:
-            outfile.write(json_object)
-    if plot:
-        plotChordResults()
+            results = {}
 
-
-# Updates a dictionary with new key+values
-def createJson(dict, id: str, first, second, fName:str, sName:str):
-    s = {}
-    s[fName] = first
-    s[sName] = second
-    dict[id] = s
-
-
-# Stores accuracy of Chord Algorithm and Neural Network for song
-def createResults(dict, id: str, algorithm: float = None, neural: float = None):
-    s = {}
-    s["algo-accuracy"] = algorithm
-    s["neural-accuracy"] = neural
-    dict[id] = s
-
-
-# Compares two chord arrays based on matching indexes with their timestamp arrays
-def compareChords(gt_timestamp, gt_chord, alg_timestamp, alg_chord):
-    results = 0
-    for idx, timestamp in enumerate(gt_timestamp):
-        near = find_nearest(alg_timestamp, timestamp)
-        algoChord = alg_chord[near]
-        # print(timestamp)              # Debug
-        # print(algoChord + " " + gt_chord[idx])
-        if algoChord == gt_chord[idx]:
-            results += 1
-    # Return number of correct guesses divided by total guesses - can improve
-    temp = results / len(gt_chord)
-    return temp
+        for id in dataset:
+            if id not in results.keys() or force:
+                print("Evaluating beats for id: " + id)
+                try:
+                    preprocessing.downloadAudio(id)
+                except dict.YoutubeError:
+                    return    
+                beatRecognizer = beat_algorithm.BeatRecognizer(id)
+                beatRecognizer.run()
+                accuracy, beatresults = self.evaluateBeats(dataset[id]["beats"], beatRecognizer.beats)
+                self.createJson(beat_data, id, accuracy, beatresults, "accuracy", "beat_results")
+                json_object = json.dumps(beat_data[id], indent=3)
+                with open(dict.RESULTS_BEATS_PATH + id + '.json', "w+") as outfile:
+                    outfile.write(json_object)
+                os.remove(dict.getNativeAudioPath(id))
+                os.remove(dict.getModifiedAudioPath(id))
+            else:
+                print("Data already exists for " + id + ", skipping to next...")
+        self.output(chorddata=None, beatdata=results, detailed=None)
+        if verbose:
+            plotBeatResults()
+            with open(dict.BEAT_RESULTS_PATH, 'r') as f:
+                printing = json.loads(f.read())
+            df = pd.DataFrame.from_dict(printing)
+            df['average'] = df[:1].mean(axis=1)
+            print("Average accuracy: " + str(df['average']['accuracy']))
 
 
 # finds closest value in array
@@ -221,53 +252,6 @@ def find_nearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return idx
-
-
-# Processes a song and stores the results
-def processChords(processed_beats, song_buffer, dataset, beat_chord_aggregate):
-    try:
-        preprocessing.downloadAudio(id)
-    except dict.YoutubeError:
-        return
-    chordRecognizer = chord_algorithm.ChordRecognizer(id)
-    newBeats = np.array(dataset[id]["beats"])
-    newBeats = newBeats[newBeats <= librosa.get_duration(filename=dict.getNativeAudioPath(id))]   # trim timestamps
-    chordRecognizer.run(beats=newBeats, verbose=True)
-    createJson(beat_chord_aggregate, id, chordRecognizer.chords.tolist(), newBeats.tolist(), "chords", "beats")
-    result = compareChords(newBeats, dataset[id]["chords"], newBeats, chordRecognizer.chords)
-    # Save
-    createJson(processed_beats, id, dataset[id]["chords"], newBeats.tolist(), "chords", "beats")    # Update processed dataclear
-    json_object = json.dumps(processed_beats[id], indent=3)
-    with open(dict.TRIMMED_SONGS_PATH + id + '.json', "w+") as outfile:
-        outfile.write(json_object)
-
-    createJson(song_buffer, id, chordRecognizer.chords.tolist(), newBeats.tolist(), "chords", "beats")
-    json_object = json.dumps(song_buffer[id], indent=3)
-    with open(dict.RESULTS_SONG_PATH + id + '.json', "w+") as outfile:
-        outfile.write(json_object)
-
-    os.remove(dict.getNativeAudioPath(id))
-    os.remove(dict.getModifiedAudioPath(id))
-    return result
-
-
-# records the batch processing results into CSV
-def output(chorddata = None, beatdata = None, detailed = None):
-    if chorddata:
-        df = pd.DataFrame(list(chorddata.items()), columns = ['id', 'result'])   # get it in dataframe form
-        aggregate = pd.cut(df['result'], bins = pd.interval_range(start=0, end=100, periods=10)).value_counts()
-        with open(dict.CHORDRESULTS_CSV_PATH, "w") as f:
-            aggregate.to_csv(f)
-    if detailed:
-        json_object = json.dumps(detailed, indent=3)
-        with open(dict.DETAILED_RESULTS_PATH, "w+") as outfile:
-            outfile.write(json_object)
-    if beatdata:
-        df = pd.DataFrame(list(beatdata.items()), columns = ['id', 'result'])
-        #print(df)
-        aggregate = pd.cut(df['result'].str['accuracy'], bins = pd.interval_range(start=0, end=100, periods=10)).value_counts()
-        with open(dict.BEATRESULTS_CSV_PATH, "w") as f:
-            aggregate.to_csv(f)
 
 
 # Plot the results into bins based on %accuracy
@@ -311,78 +295,6 @@ def previous_and_next(some_iterable):
     prevs = chain([None], prevs)
     nexts = chain(islice(nexts, 1, None), [None])
     return zip(prevs, items, nexts)
-
-
-def processBeats(dataset, id):
-    beatRecognizer = beat_algorithm.BeatRecognizer(id)
-    beatRecognizer.run()
-    return evaluateBeats(dataset, beatRecognizer.beats)
-
-
-def evaluateBeats(dataset:float, algorithm:float, verbose = False):
-    beatAccuracy = []
-    for previous, item, nxt in previous_and_next(dataset):
-        nearIdx = find_nearest(algorithm, item)
-        distance = item - algorithm[nearIdx]
-        if nxt == None:
-            nxt = previous
-        if previous == None:
-            previous = nxt
-        if distance >= 0:
-            control = abs(item - previous)
-        else:
-            control = abs(item - nxt)
-        # We halve control - at normal, it finds a value matching the control point
-        # to be 0% accurate, even though it's 100% accurate to the control.
-        beatAccuracy.append(100-abs((distance / (control/2))*100))
-        if algorithm[nearIdx] == algorithm[-1]:  # Indicates dataset is longer
-            break
-    meanAccuracy = mean(beatAccuracy)
-    if verbose:
-        print("The average accuracy is: ", meanAccuracy)
-    return meanAccuracy, beatAccuracy
-
-
-def beatProcess(force:bool = False, verbose:bool = False):
-    with open(dict.PROCESSED_JSON_PATH, 'r') as f:
-        dataset = json.loads(f.read())
-    if not os.path.exists(dict.RESULTS_BEATS_PATH):
-        os.makedirs(dict.RESULTS_BEATS_PATH)
-    beat_data = {}
-    updateJson(dict.BEAT_RESULTS_PATH, dict.RESULTS_BEATS_PATH) # avoid error on nonexistent file here
-    beatSize = os.path.getsize(dict.BEAT_RESULTS_PATH)
-    if beatSize != 0:
-        with open(dict.BEAT_RESULTS_PATH, 'r') as f:
-            results = json.loads(f.read())
-    else:
-        results = {}
-
-    for id in dataset:
-        if id not in results.keys() or force:
-            print("Evaluating beats for id: " + id)
-            try:
-                preprocessing.downloadAudio(id)
-            except dict.YoutubeError:
-                return    
-            beatRecognizer = beat_algorithm.BeatRecognizer(id)
-            beatRecognizer.run()
-            accuracy, beatresults = evaluateBeats(dataset[id]["beats"], beatRecognizer.beats)
-            createJson(beat_data, id, accuracy, beatresults, "accuracy", "beat_results")
-            json_object = json.dumps(beat_data[id], indent=3)
-            with open(dict.RESULTS_BEATS_PATH + id + '.json', "w+") as outfile:
-                outfile.write(json_object)
-            os.remove(dict.getNativeAudioPath(id))
-            os.remove(dict.getModifiedAudioPath(id))
-        else:
-            print("Data already exists for " + id + ", skipping to next...")
-    output(chorddata=None, beatdata=results, detailed=None)
-    if verbose:
-        plotBeatResults()
-        with open(dict.BEAT_RESULTS_PATH, 'r') as f:
-            printing = json.loads(f.read())
-        df = pd.DataFrame.from_dict(printing)
-        df['average'] = df[:1].mean(axis=1)
-        print("Average accuracy: " + str(df['average']['accuracy']))
 
 
 def plotBeatResults():
